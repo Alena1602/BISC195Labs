762004eb7f4fa111ea9a78d5702b5cd51c599dbe
Alena <hlokhman@wellesley.edu>
2021-06-29 12:09:39 -0400

    src/needleman_wunch

--- src/needleman_wunch.jl
+++ src/needleman_wunch.jl
@@ -5,11 +5,11 @@
 
 Your documentation here.
 """
-function nwscore(base1::Char, base2::Char)
+function nwscore(base1::Char Score, base2::Char Score)
     
 end
 
-nwscore(base::Char, ::Nothing) = -1
-nwscore(::Nothing, base::Char) = nwscore(base, nothing)
+nwscore(base::Char Score, ::Nothing) = -1
+nwscore(::Nothing, base::Char Score) = nwscore(base, nothing)
 
 nwscore(::Nothing, ::Nothing) = throw(ArgumentError("Score for two gaps is not defined"))
